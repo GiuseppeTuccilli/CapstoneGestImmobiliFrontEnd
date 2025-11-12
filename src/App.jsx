fcae4e26488e658227f6ef9021edf28f0637b270
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2025-11-12 14:32:33 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,21 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import { Provider } from "react-redux";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MyNavbar from "./components/MyNavbar";
+import RegisterForm from "./components/RegisterPage";
 
 function App() {
   return (
     <>
-      <BrowserRouter></BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/home" element={<MyNavbar />} />
+          <Route path="/immobili" element={<MyNavbar />} />
+          <Route path="/clienti" element={<MyNavbar />} />
+          <Route path="/visite" element={<MyNavbar />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/MyNavbar.jsx
@@ -0,0 +1,51 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
+
+function MyNavbar() {
+  return (
+    <nav className="navbar navbar-expand-lg ">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler text-light"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 w-100 justify-content-around">
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to={"/home"}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/immobili"}>
+                Immobili
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/clienti"}>
+                Clienti
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/visite"}>
+                Visite
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default MyNavbar;
