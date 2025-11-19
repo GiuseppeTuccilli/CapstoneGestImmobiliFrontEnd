b7a40b5530df1f7cf057d6d6c848882c69562cbb
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2025-11-19 11:27:24 +0100

    navbar link active

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -2,9 +2,11 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function MyNavbar() {
+  const location = useLocation();
+  console.log(location.pathname);
   return (
     <nav className="navbar navbar-expand-lg sticky-top ">
       <div className="container-fluid">
@@ -21,23 +23,47 @@ function MyNavbar() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0 w-100 justify-content-around">
-            <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to={"/"}>
+            <li className="nav-item ">
+              <Link
+                className={
+                  "nav-link " + (location.pathname === "/" && " active")
+                }
+                aria-current="page"
+                to={"/"}
+              >
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to={"/immobili"}>
+              <Link
+                className={
+                  "nav-link " +
+                  (location.pathname.startsWith("/immobili") && " active")
+                }
+                to={"/immobili"}
+              >
                 Immobili
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to={"/clienti"}>
+              <Link
+                className={
+                  "nav-link " +
+                  (location.pathname.startsWith("/clienti") && " active")
+                }
+                to={"/clienti"}
+              >
                 Clienti
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to={"/visite"}>
+              <Link
+                className={
+                  "nav-link " +
+                  (location.pathname.startsWith("/visite") && " active")
+                }
+                to={"/visite"}
+              >
                 Visite
               </Link>
             </li>
