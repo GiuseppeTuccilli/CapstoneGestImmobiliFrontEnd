92a63a1569d1b528ac4e1a7fc1ad85c9a17b1cfb
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2026-01-03 19:25:02 +0100

    back to lacal host

--- src/variabili.js
+++ src/variabili.js
@@ -1,3 +1,4 @@
-const base = "https://juicy-estelle-giuseppetuccilli-7ea7e3a5.koyeb.app";
+//const base = "https://juicy-estelle-giuseppetuccilli-7ea7e3a5.koyeb.app";
+const base = "http://localhost:8080";
 
 export default base;
